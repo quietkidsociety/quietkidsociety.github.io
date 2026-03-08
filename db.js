f7122e499ba3a5ee3210c25f877070233b40f80c
ItsCentralize <itscentralize@gmail.com>
2026-03-09 00:28:15 +0800

    Add files via upload

---
+++ db.js
@@ -0,0 +1,29 @@
+const qksMembers = {
+  "itscentralize": {
+    qksMemberName: "ItsCentralize",
+    qksMemberLinks: [
+      ["Email", "itscentralize@gmail.com", "mailto:itscentralize@gmail.com"]
+      ["Instagram", "@itscentralize", "https://www.instagram.com/itscentralize/"]
+    ],
+    qksMemberAbout: ""
+  }, "itscentralize": {
+    qksMemberName: "CardPartyPaco",
+    qksMemberLinks: [
+      ["Instagram", "@cardpartypaco", "https://www.instagram.com/cardpartypaco/"]
+    ],
+    qksMemberAbout: ""
+  }
+};
+const qksStreaks = [
+  {
+    qksStreakName: "",
+    qksStreakHolder: "itscentralize",
+    qksStreakStart: new Date("27 May 2024 00:00:00.000 GMT+8"),
+    qksStreakEnd: new Date(0)
+  }, {
+    qksStreakName: "",
+    qksStreakHolder: "cardpartypaco",
+    qksStreakStart: new Date("18 July 2025 00:00:00.000 GMT+8"),
+    qksStreakEnd: new Date(0)
+  }
+]
---
+++ lib.js
@@ -0,0 +1,46 @@
+function qksStreakDuration(qksStreak) {
+  if (qksStreak.qksStreakEnd < qksStreak.qksStreakStart) {
+    return new Date() - qksStreak.qksStreakStart;
+  } else {
+    return qksStreak.qksStreakEnd - qksStreak.qksStreakStart;
+  }
+}
+
+function qksFormatDuration(qksDuration) {
+  var qksDurationMagnitude = Math.abs(qksDuration);
+  var qksDurationDirection = qksDuration === 0 ? 0 : qksDuration / qksDurationMagnitude;
+  return `${
+    qksDurationDirection === -1 ? "-" : ""
+  }${
+    String(parseInt(qksDurationMagnitude / 86400000)).padStart(3, "0")
+  }:${
+    String(parseInt(qksDurationMagnitude % 86400000 / 3600000)).padStart(2, "0")
+  }:${
+    String(parseInt(qksDurationMagnitude % 3600000 / 60000)).padStart(2, "0")
+  }:${
+    String(parseInt(qksDurationMagnitude % 60000 / 1000)).padStart(2, "0")
+  }.${
+    String(parseInt(qksDurationMagnitude % 1000)).padStart(3, "0")
+  }`;
+}
+const qksFormatimeMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov",
+  "Dec"];
+
+function qksFormatTime(qksTime) {
+  const qksTimeCST = new Date(qksTime.getTime() + 28800000);
+  return `${
+    String(qksTimeCST.getUTCDate()).padStart(2, "0")
+  } ${
+    qksFormatimeMonths[qksTimeCST.getUTCMonth()]
+  } ${
+    qksTimeCST.getUTCFullYear()
+  } ${
+    String(qksTimeCST.getUTCHours()).padStart(2, "0")
+  }:${
+    String(qksTimeCST.getUTCMinutes()).padStart(2, "0")
+  }:${
+    String(qksTimeCST.getUTCSeconds()).padStart(2, "0")
+  }.${
+    String(qksTimeCST.getUTCMilliseconds()).padStart(3, "0")
+  } GMT+08`;
+}
