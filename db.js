6b6eae3158a4f385cbfc73db0db7900b2eff55cf
ItsCentralize <itscentralize@gmail.com>
2026-03-26 20:39:21 +0800

    Add files via upload

---
+++ db.js
@@ -0,0 +1,62 @@
+const qksMembers = {
+  "itscentralize": {
+    qksMemberName: "ItsCentralize",
+    qksMemberLinks: [
+      ["Email", "itscentralize@gmail.com", "mailto:itscentralize@gmail.com"],
+    ],
+    qksMemberAbout: ""
+  }, "shadowerrorblob12": {
+    qksMemberName: "ShadowErrorBlob12",
+    qksMemberLinks: [],
+    qksMemberAbout: ""
+  }, "3rr0ruser": {
+    qksMemberName: "Ner0",
+    qksMemberLinks: [],
+    qksMemberAbout: ""
+  }, "tofu_1o": {
+    qksMemberName: "Tofu",
+    qksMemberLinks: [],
+    qksMemberAbout: ""
+  }, "cardpartypaco": {
+    qksMemberName: "Taco",
+    qksMemberLinks: [],
+    qksMemberAbout: ""
+  }, "spaghetti_wizard62": {
+    qksMemberName: "Spinzitsu Mastaa",
+    qksMemberLinks: [],
+    qksMemberAbout: ""
+  }
+};
+const qksStreaks = [
+  {
+    qksStreakName: "",
+    qksStreakHolder: "itscentralize",
+    qksStreakStart: new Date("2024-05-21T00:00:00.000+08:00"),
+    qksStreakEnd: new Date(0)
+  }, {
+    qksStreakName: "",
+    qksStreakHolder: "cardpartypaco",
+    qksStreakStart: new Date("2025-07-12T00:00:00.000+08:00"),
+    qksStreakEnd: new Date(0)
+  }, {
+    qksStreakName: "",
+    qksStreakHolder: "tofu_1o",
+    qksStreakStart: new Date("2025-09-04T00:00:00.000+08:00"),
+    qksStreakEnd: new Date(0)
+  }, {
+    qksStreakName: "",
+    qksStreakHolder: "shadowerrorblob12",
+    qksStreakStart: new Date("2025-08-22T00:00:00.000+08:00"),
+    qksStreakEnd: new Date(0)
+  }, {
+    qksStreakName: "",
+    qksStreakHolder: "3rr0ruser",
+    qksStreakStart: new Date("2025-12-05T00:00:00.000+08:00"),
+    qksStreakEnd: new Date("2026-03-01T00:07:30.000+08:00")
+  }, {
+    qksStreakName: "",
+    qksStreakHolder: "spaghetti_wizard62",
+    qksStreakStart: new Date("2026-03-27T00:00:00.000+11:00"),
+    qksStreakEnd: new Date(0)
+  }, 
+]
